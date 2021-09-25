408ee70b54daf5c1376219a26a4bb7e8be6c1d17
Phong Nguyen <nguyenphuphong@gmail.com>
2021-09-25 12:57:01 +0700

    nav to js

---
+++ js/lesson-navigator.js
@@ -0,0 +1,5 @@
+var lesson_navigator_array = [
+	['【00】前言', '00.html'],
+	['【01】你好', '01.html'],
+	['【02】汉语不太难', '02.html']
+];
--- js/main.js
+++ js/main.js
@@ -10,3 +10,21 @@ function toogle_nav_display () {
 		nav_content.style.display = "none";
 	}
 }
+
+function build_lesson_navigation(page) {
+	var build_string = "";
+	
+	for (var i = 0; i < lesson_navigator_array.length; i++) {
+		var lesson_navigator_title = lesson_navigator_array[i][0];
+		var lesson_navigator_url = lesson_navigator_array[i][1];
+		
+		if (page == i) {
+			build_string += "<li><a href=\"" + lesson_navigator_url + "\">" + lesson_navigator_title + "</a></li>";
+		} else {
+			build_string += "<li class=\"active\"><a href=\"" + lesson_navigator_url + "\">" + lesson_navigator_title + "</a></li>";
+		}
+	}
+	
+	var nav_content_list = document.getElementById("nav-content-list");
+	nav_content_list.innerHTML = build_string;
+}
