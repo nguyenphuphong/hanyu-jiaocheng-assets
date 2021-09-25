3babf400a933565c66e1cbbac5a89c41d29fcbed
Phong Nguyen <nguyenphuphong@gmail.com>
2021-09-25 15:05:26 +0700

    index js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var donation_array = [
-	"Website này hoàn toàn miễn phí cho mục đích học tiếng Trung, không đính kèm quảng cáo. Tuy nhiên, việc duy trì hoạt động của website thì không miễn phí. Nếu bạn cảm thấy website này có ích cho việc học tập của bản thân, bạn có thể hổ trợ tôi một tách cà phê để duy trì hoạt động của website này.",
+	"Website này hoàn toàn miễn phí cho mục đích học tiếng Trung, không đính kèm quảng cáo. Tuy nhiên, việc thuê máy chủ để duy trì hoạt động của website thì không miễn phí. Nếu bạn cảm thấy website này có ích cho việc học tập của bản thân, bạn có thể hổ trợ tôi một tách cà phê để duy trì hoạt động của website này.",
 	"Ngân hàng Vietcombank <br /> CTK: Nguyễn Phú Phong <br /> STK: 0721005112695",
 	"Nội dung trên website này là những ghi chú khi tôi học bằng bộ sách <b>\"Giáo trình Hán Ngữ - phiên bản mới\"</b>. Do đó nó có thể không phù hợp với bất kì phương pháp sư phạm nào, và mục đích của website này cũng không phải là để thay thế, cạnh tranh với bộ sách này. Để tăng tính hiệu quả của việc học bạn nên mua và sử dụng bộ sách <b>\"Giáo trình Hán Ngữ\"</b>.",
 	"Nếu bạn muốn góp ý nhằm cải thiện website này xin vui lòng liên hệ tôi qua địa chỉ email. <br /> <b>nguyenphuphong@gmail.com</b>",
