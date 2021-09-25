801732ffdeb043e1df71869cdd8cce7026de3a4f
Phong Nguyen <nguyenphuphong@gmail.com>
2021-09-25 17:41:21 +0700

    in

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@ var donation_array = [
 	"Ngân hàng Vietcombank <br /> CTK: Nguyễn Phú Phong <br /> STK: 0721005112695",
 	"Nội dung trên website này là những ghi chú khi tôi học bằng bộ sách <b>\"Giáo trình Hán Ngữ - phiên bản mới\"</b>. Do đó nó có thể không phù hợp với bất kì phương pháp sư phạm nào, và mục đích của website này cũng không phải là để thay thế, cạnh tranh với bộ sách này. Để tăng tính hiệu quả của việc học bạn nên mua và sử dụng bộ sách <b>\"Giáo trình Hán Ngữ\"</b>.",
 	"Nếu bạn muốn góp ý nhằm cải thiện website này xin vui lòng liên hệ tôi qua địa chỉ email. <br /> <b>nguyenphuphong@gmail.com</b>",
-	"<b>Chân thành cảm ơn và chúc bạn thành công trong học tập.</b>"
+	"<b>Chân thành cảm ơn và chúc bạn thành công.</b>"
 ];
