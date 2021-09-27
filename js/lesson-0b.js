0e5957c4ee214e422b036fd87998b962301be8c6
Phong Nguyen <nguyenphuphong@gmail.com>
2021-09-27 15:18:26 +0700

    2b

--- js/lesson-0b.js
+++ js/lesson-0b.js
@@ -1,10 +1,3 @@
-var pinyin_array = [
-	"Thời xa xưa thì chưa bao giờ xuất hiện khái niệm phiên âm Pinyin trong đời sống của người dân Trung Hoa. Cũng không có khái niệm bảng phiên âm tiếng Trung. Vì đơn giản vì Pinyin mới xuất hiện và được phê chuẩn năm 1958, cách đây chưa quá xa xôi.",
-	"Còn chữ Trung quốc thuộc nhóm chữ tượng hình, viết loằng ngoằng làm gì có bảng chữ cái mà áp dụng theo. Họa chăng lắm người ta tổng hợp các từ có nét giống nhau lại thành bộ thủ. Nhưng 214 bộ thủ xét về quy tắc thì không thể nào chặt chẽ và chính xác như việc ghép các từ trong chữ latinh.",
-	"Phiên âm pinyin chính là việc sử dụng các chữ cái latinh để mô phỏng lại cách phát âm trong hán ngữ. Việc này giúp cho người mới sẽ dễ học hơn, dễ bắt chước hơn nhất là người nước ngoài.",
-	"Pinyin nói nôm na nó giống cấu trúc của tiếng Việt. Sử dụng nguyên âm để ghép với phụ âm vào tạo ra cách đọc. Chúng ta nên học pinyin vì nó còn dể dàng để sử dụng các kiểu gõ phím trên điện thoại và máy tính."
-];
-
 var initial_01_array = [
 	[
 		[50, 'b'], [50, 'p'], [50, 'm'], [50, 'f'], [50, 'd'], [50, 't'], [50, 'n'], [50, 'l'], [50, 'g'], [50, 'k'], [50, 'h']
