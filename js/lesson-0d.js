0c011f7ded01076a93e9e8ab76aa6c7d5029a8f7
Phong Nguyen <nguyenphuphong@gmail.com>
2021-09-28 16:05:51 +0700

    0d

--- js/lesson-0d.js
+++ js/lesson-0d.js
@@ -1,131 +1,227 @@
 var lyric_01_array = [
 	[
-		[300, "哪怕事事都大度宽容"], [350, "nǎpà shì shì dū dà dù kuānróng"]
+		[300, "哪怕事事都大度宽容"], ["minus-320", "nǎpà shì shì dū dà dù kuānróng"]
 	],
 	[
-		[300, "伤害又何曾停止"], [350, "shānghài yòu hécéng tíngzhǐ"]
+		[300, "伤害又何曾停止"], ["minus-320", "shānghài yòu hécéng tíngzhǐ"]
 	],
 	[
-		[300, "哪怕事事都温柔忍耐"], [350, "nǎpà shì shì dōu wēnróu rěnnài"]
+		[300, "哪怕事事都温柔忍耐"], ["minus-320", "nǎpà shì shì dōu wēnróu rěnnài"]
 	],
 	[
-		[300, "难过又何曾减少"], [350, "nánguò yòu hécéng jiǎnshǎo"]
+		[300, "难过又何曾减少"], ["minus-320", "nánguò yòu hécéng jiǎnshǎo"]
 	],
 	[
-		[300, "善良的你掏心掏肺"], [350, "shànliáng de nǐ tāoxīn tāo fèi"]
+		[300, "善良的你掏心掏肺"], ["minus-320", "shànliáng de nǐ tāoxīn tāo fèi"]
 	],
 	[
-		[300, "想看你出丑的人却太多"], [350, "xiǎng kàn nǐ chūchǒu de rén què tài duō"]
+		[300, "想看你出丑的人却太多"], ["minus-320", "xiǎng kàn nǐ chūchǒu de rén què tài duō"]
 	],
 	[
-		[300, "你自己也不好过"], [350, "nǐ zìjǐ yě bù hǎoguò"]
+		[300, "你自己也不好过"], ["minus-320", "nǐ zìjǐ yě bù hǎoguò"]
 	],
 	[
-		[300, "却要替别人的故事感动"], [350, "què yào tì biérén de gùshì gǎndòng"]
+		[300, "却要替别人的故事感动"], ["minus-320", "què yào tì biérén de gùshì gǎndòng"]
 	],
 	[
-		[300, "月亮月亮你别睡"], [350, "yuèliàng yuèliàng nǐ bié shuì"]
+		[300, "月亮月亮你别睡"], ["minus-320", "yuèliàng yuèliàng nǐ bié shuì"]
 	],
 	[
-		[300, "迷茫的人他已酒醉"], [350, "mímáng de rén tā yǐ jiǔ zuì"]
+		[300, "迷茫的人他已酒醉"], ["minus-320", "mímáng de rén tā yǐ jiǔ zuì"]
 	],
 	[
-		[300, "思念的人已经不在"], [350, "sīniàn de rén yǐjīng bùzài"]
+		[300, "思念的人已经不在"], ["minus-320", "sīniàn de rén yǐjīng bùzài"]
 	],
 	[
-		[300, "人生不过一堆堆的顾念"], [350, "rénshēng bùguò yī duī duī de gùniàn"]
+		[300, "人生不过一堆堆的顾念"], ["minus-320", "rénshēng bùguò yī duī duī de gùniàn"]
 	],
 	[
-		[300, "月亮月亮你别睡"], [350, "yuèliàng yuèliàng nǐ bié shuì"]
+		[300, "月亮月亮你别睡"], ["minus-320", "yuèliàng yuèliàng nǐ bié shuì"]
 	],
 	[
-		[300, "捱过这段艰难日子"], [350, "áiguò zhè duàn jiān nàn rìzi"]
+		[300, "捱过这段艰难日子"], ["minus-320", "áiguò zhè duàn jiān nàn rìzi"]
 	],
 	[
-		[300, "想起来也不过如此"], [350, "xiǎng qǐlái yě bùguò rúcǐ"]
+		[300, "想起来也不过如此"], ["minus-320", "xiǎng qǐlái yě bùguò rúcǐ"]
 	],
 	[
-		[300, "虚伪的酒我再也不接"], [350, "xūwèi de jiǔ wǒ zài yě bù jiē"]
+		[300, "虚伪的酒我再也不接"], ["minus-320", "xūwèi de jiǔ wǒ zài yě bù jiē"]
 	],
 	[
-		[300, "..."], [350, "..."]
+		[300, "..."], ["minus-320", "..."]
 	],
 	[
-		[300, "哪怕事事都大度宽容"], [350, "nǎpà shì shì dū dà dù kuānróng"]
+		[300, "哪怕事事都大度宽容"], ["minus-320", "nǎpà shì shì dū dà dù kuānróng"]
 	],
 	[
-		[300, "伤害又何曾停止"], [350, "shānghài yòu hécéng tíngzhǐ"]
+		[300, "伤害又何曾停止"], ["minus-320", "shānghài yòu hécéng tíngzhǐ"]
 	],
 	[
-		[300, "哪怕事事都温柔忍耐"], [350, "nǎpà shì shì dōu wēnróu rěnnài"]
+		[300, "哪怕事事都温柔忍耐"], ["minus-320", "nǎpà shì shì dōu wēnróu rěnnài"]
 	],
 	[
-		[300, "难过又何曾减少"], [350, "nánguò yòu hécéng jiǎnshǎo"]
+		[300, "难过又何曾减少"], ["minus-320", "nánguò yòu hécéng jiǎnshǎo"]
 	],
 	[
-		[300, "善良的你掏心掏肺"], [350, "shànliáng de nǐ tāoxīn tāo fèi"]
+		[300, "善良的你掏心掏肺"], ["minus-320", "shànliáng de nǐ tāoxīn tāo fèi"]
 	],
 	[
-		[300, "想看你出丑的人却太多"], [350, "xiǎng kàn nǐ chūchǒu de rén què tài duō"]
+		[300, "想看你出丑的人却太多"], ["minus-320", "xiǎng kàn nǐ chūchǒu de rén què tài duō"]
 	],
 	[
-		[300, "你自己也不好过"], [350, "nǐ zìjǐ yě bù hǎoguò"]
+		[300, "你自己也不好过"], ["minus-320", "nǐ zìjǐ yě bù hǎoguò"]
 	],
 	[
-		[300, "却要替别人的故事感动"], [350, "què yào tì biérén de gùshì gǎndòng"]
+		[300, "却要替别人的故事感动"], ["minus-320", "què yào tì biérén de gùshì gǎndòng"]
 	],
 	[
-		[300, "月亮月亮你别睡"], [350, "yuèliàng yuèliàng nǐ bié shuì"]
+		[300, "月亮月亮你别睡"], ["minus-320", "yuèliàng yuèliàng nǐ bié shuì"]
 	],
 	[
-		[300, "迷茫的人他已酒醉"], [350, "mímáng de rén tā yǐ jiǔ zuì"]
+		[300, "迷茫的人他已酒醉"], ["minus-320", "mímáng de rén tā yǐ jiǔ zuì"]
 	],
 	[
-		[300, "思念的人已经不在"], [350, "sīniàn de rén yǐjīng bùzài"]
+		[300, "思念的人已经不在"], ["minus-320", "sīniàn de rén yǐjīng bùzài"]
 	],
 	[
-		[300, "人生不过一堆堆的顾念"], [350, "rénshēng bùguò yī duī duī de gùniàn"]
+		[300, "人生不过一堆堆的顾念"], ["minus-320", "rénshēng bùguò yī duī duī de gùniàn"]
 	],
 	[
-		[300, "月亮月亮你别睡"], [350, "yuèliàng yuèliàng nǐ bié shuì"]
+		[300, "月亮月亮你别睡"], ["minus-320", "yuèliàng yuèliàng nǐ bié shuì"]
 	],
 	[
-		[300, "捱过这段艰难日子"], [350, "áiguò zhè duàn jiān nàn rìzi"]
+		[300, "捱过这段艰难日子"], ["minus-320", "áiguò zhè duàn jiān nàn rìzi"]
 	],
 	[
-		[300, "想起来也不过如此"], [350, "xiǎng qǐlái yě bùguò rúcǐ"]
+		[300, "想起来也不过如此"], ["minus-320", "xiǎng qǐlái yě bùguò rúcǐ"]
 	],
 	[
-		[300, "虚伪的酒我再也不接"], [350, "xūwèi de jiǔ wǒ zài yě bù jiē"]
+		[300, "虚伪的酒我再也不接"], ["minus-320", "xūwèi de jiǔ wǒ zài yě bù jiē"]
 	],
 	[
-		[300, "月亮月亮你别睡"], [350, "yuèliàng yuèliàng nǐ bié shuì"]
+		[300, "月亮月亮你别睡"], ["minus-320", "yuèliàng yuèliàng nǐ bié shuì"]
 	],
 	[
-		[300, "喔迷茫的人他已酒醉"], [350, "wō mímáng de rén tā yǐ jiǔ zuì"]
+		[300, "喔迷茫的人他已酒醉"], ["minus-320", "wō mímáng de rén tā yǐ jiǔ zuì"]
 	],
 	[
-		[300, "思念的人已经不在"], [350, "sīniàn de rén yǐjīng bùzài"]
+		[300, "思念的人已经不在"], ["minus-320", "sīniàn de rén yǐjīng bùzài"]
 	],
 	[
-		[300, "人生不过一堆堆的顾念"], [350, "rénshēng bùguò yī duī duī de gùniàn"]
+		[300, "人生不过一堆堆的顾念"], ["minus-320", "rénshēng bùguò yī duī duī de gùniàn"]
 	],
 	[
-		[300, "月亮月亮你别睡"], [350, "yuèliàng yuèliàng nǐ bié shuì"]
+		[300, "月亮月亮你别睡"], ["minus-320", "yuèliàng yuèliàng nǐ bié shuì"]
 	],
 	[
-		[300, "捱过这段艰难日子"], [350, "áiguò zhè duàn jiān nàn rìzi"]
+		[300, "捱过这段艰难日子"], ["minus-320", "áiguò zhè duàn jiān nàn rìzi"]
 	],
 	[
-		[300, "想起来也不过如此"], [350, "xiǎng qǐlái yě bùguò rúcǐ"]
+		[300, "想起来也不过如此"], ["minus-320", "xiǎng qǐlái yě bùguò rúcǐ"]
 	],
 	[
-		[300, "虚伪的酒我再也不接"], [350, "xūwèi de jiǔ wǒ zài yě bù jiē"]
+		[300, "虚伪的酒我再也不接"], ["minus-320", "xūwèi de jiǔ wǒ zài yě bù jiē"]
 	],
 	[
-		[300, "想起来也不过如此"], [350, "xiǎng qǐlái yě bùguò rúcǐ"]
+		[300, "想起来也不过如此"], ["minus-320", "xiǎng qǐlái yě bùguò rúcǐ"]
 	],
 	[
-		[300, "虚伪的酒我再也不接"], [350, "xūwèi de jiǔ wǒ zài yě bù jiē"]
+		[300, "虚伪的酒我再也不接"], ["minus-320", "xūwèi de jiǔ wǒ zài yě bù jiē"]
+	]
+];
+
+var lyric_01_array = [
+	[
+		[300, "只叹她回眸秋水被隐去"], ["minus-320", "zhǐ tàn tā huímóu qiūshuǐ bèi yǐn qù"]
+	],
+	[
+		[300, "只忆她点破去日苦多"], ["minus-320", "zhǐ yì tā diǎnpò qù rì kǔ duō"]
+	],
+	[
+		[300, "借三两苦酒方知离不可"], ["minus-320", "jiè sān liǎng kǔ jiǔ fāng zhī lí bùkě"]
+	],
+	[
+		[300, "只叹她将思念摇落"], ["minus-320", "zhǐ tàn tā jiāng sīniàn yáo luò"]
+	],
+	[
+		[300, "心多憔悴爱付与东流的水"], ["minus-320", "xīn duō qiáocuì ài fùyǔ dōngliú de shuǐ"]
+	],
+	[
+		[300, "舍命奉陪抵不过天公不作美"], ["minus-320", "shěmìng fèngpéi dǐ bùguò tiāngōng bù zuòměi"]
+	],
+	[
+		[300, "往事回味不过是弹指一挥"], ["minus-320", "wángshì huíwèi bùguò shì tánzhǐ yī huī"]
+	],
+	[
+		[300, "日复日望穿秋水恕我愚昧"], ["minus-320", "rì fù rì wàngchuānqiūshuǐ shù wǒ yúmèi"]
+	],
+	[
+		[300, "你爱着谁心徒留几道伤"], ["minus-320", "nǐ ài zhe shuí xīn tú liú jǐ dào shāng"]
+	],
+	[
+		[300, "我锁着眉最是相思断人肠"], ["minus-320", "wǒ suǒ zhe méi zuì shì xiāngsī duàn rén cháng"]
+	],
+	[
+		[300, "劳燕分飞寂寥的夜里泪两行"], ["minus-320", "láoyànfēnfēi jìliáo de yè·li lèi liǎng xíng"]
+	],
+	[
+		[300, "烛短遗憾长故人自难忘"], ["minus-320", "zhú duǎn yíhàn cháng gùrén zì nán wàng"]
+	],
+	[
+		[300, "你爱着谁心徒留几道伤"], ["minus-320", "nǐ ài zhe shuí xīn tú liú jǐ dào shāng"]
+	],
+	[
+		[300, "爱多可悲恨彼此天涯各一方"], ["minus-320", "ài duō kěbēi hèn bǐcǐ tiānyá gè yī fāng"]
+	],
+	[
+		[300, "冷月空对满腹愁无处话凄凉"], ["minus-320", "lěng yuè kōng duì mǎn fù chóu wú chǔ huà qīliáng"]
+	],
+	[
+		[300, "我爱不悔可孤影难成双"], ["minus-320", "wǒ ài bù huǐ kě gū yǐng nán chéngshuāng"]
+	],
+	[
+		[300, "心多憔悴爱付与东流的水"], ["minus-320", "xīn duō qiáocuì ài fùyǔ dōngliú de shuǐ"]
+	],
+	[
+		[300, "舍命奉陪抵不过天公不作美"], ["minus-320", "shěmìng fèngpéi dǐ bùguò tiāngōng bù zuòměi"]
+	],
+	[
+		[300, "往事回味不过是弹指一挥"], ["minus-320", "wángshì huíwèi bùguò shì tánzhǐ yī huī"]
+	],
+	[
+		[300, "日复日望穿秋水恕我愚昧"], ["minus-320", "rì fù rì wàngchuānqiūshuǐ shù wǒ yúmèi"]
+	],
+	[
+		[300, "你爱着谁心徒留几道伤"], ["minus-320", "nǐ ài zhe shuí xīn tú liú jǐ dào shāng"]
+	],
+	[
+		[300, "我锁着眉最是相思断人肠"], ["minus-320", "wǒ suǒ zhe méi zuì shì xiāngsī duàn rén cháng"]
+	],
+	[
+		[300, "劳燕分飞寂寥的夜里泪两行"], ["minus-320", "láoyànfēnfēi jìliáo de yè·li lèi liǎng xíng"]
+	],
+	[
+		[300, "你爱着谁心徒留几道伤"], ["minus-320", "nǐ ài zhe shuí xīn tú liú jǐ dào shāng"]
+	],
+	[
+		[300, "爱多可悲恨彼此天涯各一方"], ["minus-320", "ài duō kěbēi hèn bǐcǐ tiānyá gè yī fāng"]
+	],
+	[
+		[300, "冷月空对满腹愁无处话凄凉"], ["minus-320", "lěng yuè kōng duì mǎn fù chóu wú chǔ huà qīliáng"]
+	],
+	[
+		[300, "我爱不悔可孤影难成双"], ["minus-320", "wǒ ài bù huǐ kě gū yǐng nán chéngshuāng"]
+	],
+	[
+		[300, "你爱着谁心徒留几道伤"], ["minus-320", "nǐ ài zhe shuí xīn tú liú jǐ dào shāng"]
+	],
+	[
+		[300, "我锁着眉最是相思断人肠"], ["minus-320", "wǒ suǒ zhe méi zuì shì xiāngsī duàn rén cháng"]
+	],
+	[
+		[300, "劳燕分飞寂寥的夜里泪两行"], ["minus-320", "láoyànfēnfēi jìliáo de yè·li lèi liǎng xíng"]
+	],
+	[
+		[300, "烛短遗憾长故人自难忘"], ["minus-320", "zhú duǎn yíhàn cháng gùrén zì nán wàng"]
 	]
 ];
