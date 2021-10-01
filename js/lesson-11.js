2266a3440052fd3e11b2b2e101cd90e9cc9d0ba1
Phong Nguyen <nguyenphuphong@gmail.com>
2021-10-01 11:06:15 +0700

    10

---
+++ js/lesson-11.js
@@ -0,0 +1,50 @@
+var listening_array = [
+	["（一）这位是王教授"],
+	["（秘书给校长介绍王教授）"],
+	["秘书", "我先介绍一下儿，这位是王教授。这是马校长。", "Wǒ xiān jièshào yīxià er, zhè wèi shì wáng jiàoshòu. Zhè shì mǎ xiàozhǎng."],
+	["校长", "欢迎您，王教授。", "Huānyíng nín, wáng jiàoshòu."],
+	["王教授", "谢谢！", "Xièxiè!"],
+	["（二）我们都是留学生"],
+	["麦克", "你是留学生吗？", "Nǐ shì liúxuéshēng ma?"],
+	["玛丽", "是。", "Shì."],
+	["麦克", "罗兰也是留学生吗？", "Luólán yěshì liúxuéshēng ma?"],
+	["玛丽", "她也是留学生。我们都是留学生。", "Tā yěshì liúxuéshēng. Wǒmen dōu shì liúxuéshēng."],
+	["麦克", "张东和田芳也都是留学生吗？", "Zhāng dōng hé tiánfāng yě dūo shì liúxuéshēng ma?"],
+	["玛丽", "不，他们俩不是留学生。他们都是中国学生。", "Bù, tāmen liǎ bùshì liúxuéshēng. Tāmen dōu shì zhōngguó xuéshēng."],
+	["（三）你也是中国人吗"],
+	["爱德华", "他是中国人吗？", "Tā shì zhōngguó rén ma?"],
+	["李昌浩", "是。", "Shì."],
+	["爱德华", "你也是中国人吗？", "Nǐ yěshì zhōngguó rén ma?"],
+	["李昌浩", "不是。我是韩国人。", "Bùshì. Wǒ shì hánguó rén."],
+	["爱德华", "对不起。", "Duìbùqǐ."],
+	["李昌浩", "没什么。", "Méishénme."]
+];
+
+var vocabulary_array = [
+	['1.', '秘书', '名', 'mìshū', 'thư ký'],
+    ['2.', '先', '副', 'xiān', 'trước, trước tiên'],
+    ['3.', '介绍', '动', 'jièshào', 'giới thiệu'],
+    ['4.', '一下儿', '数量', 'yīxià er', 'một chút, một tí'],
+    ['5.', '位', '量', 'wèi', 'vị (chỉ người)'],
+    ['6.', '教授', '名', 'jiàoshòu', 'giáo sư'],
+    ['7.', '校长', '名', 'xiàozhǎng', 'hiệu trưởng'],
+    ['8.', '欢迎', '动', 'huānyíng', 'hoan nghênh'],
+    ['9.', '留学生', '名', 'liúxuéshēng', 'du học sinh'],
+    ['', '留学', '动', 'liúxué', 'du học'],
+    ['10.', '也', '副', 'yě', 'cũng'],
+    ['11.', '我们', '代','wǒmen', 'chúng tôi'],
+    ['', '你们', '代', 'nǐmen', 'các bạn, các anh, các chị'],
+    ['', '他们', '代', 'tāmen', 'bọn họ'],
+    ['12.', '都', '副', 'dōu', 'đều'],
+    ['13.', '和', '连', 'hé', 'và'],
+    ['14.', '俩', '数', 'liǎ', 'hai'],
+    ['15.', '学生', '名', 'xuéshēng', 'học sinh'],
+    ['16.', '没什么', '', 'méishénme', 'không có gì']
+];
+
+var name_array = [
+	["马", "mǎ", "Họ Mã"],
+	["田芳", "tián fāng", "Điền Phương"],
+	["罗兰", "luólán", "Roland"],
+	["爱德华", "àidéhuá", "Edward"]
+];
