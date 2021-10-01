a7993abaff02dfa938b5966bf0f88b2ccd277365
Phong Nguyen <nguyenphuphong@gmail.com>
2021-10-01 11:28:10 +0700

    12

---
+++ js/lesson-12.js
@@ -0,0 +1,40 @@
+var listening_array = [
+	["（一）你在哪儿学习汉语"],
+	["A", "你在哪儿学习汉语？", "Nǐ zài nǎ'er xuéxí hànyǔ?"],
+	["B", "在北京语言大学。", "Zài běijīng yǔyán dàxué."],
+	["A", "你们的老师怎么样？", "Nǐmen de lǎoshī zěnme yàng?"],
+	["B", "很好！", "Hěn hǎo!"],
+	["A", "你觉得学习韩语难吗？", "Nǐ juédé xuéxí hányǔ nán ma?"],
+	["B", "我觉得语法不太难。听和说也比较容易，但是读和写很难。", "Wǒ juédé yǔfǎ bù tài nán. Tīng hé shuō yě bǐjiào róngyì, dànshì dú hé xiě hěn nán."],
+	["（二）你们的老师是谁"],
+	["A", "我给你们介绍一下儿，这位是新同学，是我的同屋。", "Wǒ gěi nǐmen jièshào yīxià er, zhè wèi shì xīn tóngxué, shì wǒ de tóng wū."],
+	["B", "你在哪个班学习？", "Nǐ zài nǎge bān xuéxí?"],
+	["C", "在103班。", "Zài yāo líng sān bān."],
+	["B", "你们的老师是谁？", "Nǐmen de lǎoshī shì shéi?"],
+	["C", "我们的老师是林老师。", "Wǒmen de lǎoshī shì lín lǎoshī."]
+];
+
+var vocabulary_array = [
+	['1.', '语言', '名', 'yǔyán', 'ngôn ngữ'],
+    ['2.', '大学', '名', 'dàxué', 'trường đại học'],
+    ['3.', '怎么样', '代', 'zěnme yàng', 'như thế nào'],
+    ['4.', '觉得', '动', 'juédé', 'cảm thấy, cho rằng'],
+    ['5.', '语法', '名', 'yǔfǎ', 'ngữ pháp'],
+    ['6.', '听', '动', 'tīng', 'nghe'],
+    ['7.', '说', '动', 'shuō', 'nói'],
+    ['8.', '比较', '副, 动', 'bǐjiào', 'liên tưởng, so sánh'],
+    ['9.', '容易', '形', 'róngyì', 'dể, dể dàng'],
+    ['10.', '读', '动', 'dú', 'đọc'],
+    ['11.', '写', '动', 'xiě', 'viết'],
+    ['12.', '但是', '连', 'dànshì', 'nhưng'],
+    ['13.', '给', '介', 'gěi', 'tới, cho'],
+    ['14.', '新', '形', 'xīn', 'mới'],
+    ['15.', '同学', '名', 'tóngxué', 'bạn học, bạn cùng lớp'],
+    ['16.', '同屋', '名', 'tóng wū', 'bạn cùng phòng'],
+    ['17.', '班', '名', 'bān', 'lớp']
+];
+
+var name_array = [
+	["北京语言大学", "běijīng yǔyán dàxué", "Đại học ngôn ngữ Bắc Kinh"],
+	["林", "lín", "Họ Lâm"]
+];
